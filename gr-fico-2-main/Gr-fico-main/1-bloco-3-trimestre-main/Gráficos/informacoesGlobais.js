86c173721bb020a76920bcf808f19e6df50002a0
LanaLo19 <130603722+LanaLo19@users.noreply.github.com>
2024-10-02 11:30:12 -0300

    Add files via upload

---
+++ gr-fico-2-main/Gr-fico-main/1-bloco-3-trimestre-main/Gráficos/informacoesGlobais.js
@@ -0,0 +1,19 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9)
+    const pessoasMundo = (dados.total_pessoas_mundo/1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) *100)
+    const porcentagemConectada = ((pessoasConectadas/pessoasMundo)*100).toFixed(2)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Doença viral transmitida por mosquitos que ocorre em áreas tropicais e subtropicais.
+Pessoas infectadas com o vírus pela segunda vez têm um risco significativamente maior de desenvolver doença grave. `
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
+}
+visualizarInformacoesGlobais()
+
---
+++ gr-fico-2-main/Gr-fico-main/1-bloco-3-trimestre-main/Gráficos/quantidadeUsuarios.js
@@ -0,0 +1,60 @@
+import { getCSS, tickConfig } from "./common.js "
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ['2018','2019','2020','2021','2022', '2023',]
+    const quantidadeUsuarios = ['14540','5181','12012', '3145', '37317', '18000']
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-barra')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title: {
+            text: 'Número de casos no Paraná:',
+            font: {
+                color: getCSS('--cor-principal'),
+                family: getCSS('--fonte'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Anos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Números de casos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+
+        }
+
+    }
+    const grafico = document.createElement('div')
+    grafico.className - 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+
+}
+
+quantidadeUsuarios()
+
